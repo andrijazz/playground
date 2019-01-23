dfe2008f6c6b561b7089a99ed2b69ceec2e7ad33
andrijazz <a.djurisic@yahoo.com>
2019-01-23 17:47:05 +0100

    check gpu added

--- fcn/utils.py
+++ fcn/utils.py
@@ -269,3 +269,12 @@ def collect(images_path):
             for py in range(image.shape[1]):
                 classes.add(tuple(image[px, py]))
     return classes
+
+
+def check_gpu():
+    with tf.device('/gpu:0'):
+        a = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[2, 3], name='a')
+        b = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[3, 2], name='b')
+        c = tf.matmul(a, b)
+    with tf.Session() as sess:
+        print(sess.run(c))
