cbe8e810e591fb980a7951c51bb207483972ceb6
andrijazz <a.djurisic@yahoo.com>
2019-02-25 18:55:32 +0100

    paths fixes

--- fcn/settings.py
+++ fcn/settings.py
@@ -5,10 +5,12 @@ CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
 ROOT_DIR = os.path.abspath(os.path.join(CURRENT_DIR, os.pardir))
 sys.path.insert(0, ROOT_DIR)
 
-DATASET_DIR = "/mnt/datasets"
 MODEL_NAME = "fcn"
-LOG_DIR = "/mnt/log/" + MODEL_NAME
 LOG_FILENAME = "fcn"
 CONFIG_FILENAME = "config.json"
+DATA_DRIVE = "/datadrive"
+
+DATASET_DIR = DATA_DRIVE + "/datasets"
+LOG_DIR = DATA_DRIVE + "/log" + "/" + MODEL_NAME
 debug = False
 
