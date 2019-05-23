e9cf19c6ba5976bf4c117b038991343db1f062cd
andrijazz <a.djurisic@yahoo.com>
2019-05-23 18:34:52 +0200

    run training fixes

--- fcn/run.sh
+++ fcn/run.sh
@@ -2,11 +2,15 @@
 
 # notes: ...
 python train.py \
-    --batch_size 2 \
-    --dataset cityscapes \
-    --learning_rate 0.00001 \
-    --num_epochs 50 \
-    --keep_prob 0.5 \
-    --init_weights True \
-    --gpu 1 \
-    --model fcn8
+    --checkpoint='' \
+    --model='fcn8' \
+    --dataset='kitti_semantics' \
+    --batch_size=5 \
+    --num_epochs=50 \
+    --learning_rate=1e-3 \
+    --gpu=1 \
+    --keep_prob=0.5 \
+    --weights='vgg16_weights.npz' \
+    --save_on=500 \
+    --log_on=5 \
+    --debug=5
