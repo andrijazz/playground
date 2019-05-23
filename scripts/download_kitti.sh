6de065b3cfd1bac6bfe28208187c62f84ebc6420
andrijazz <a.djurisic@yahoo.com>
2019-05-23 17:11:40 +0200

    fix download kitti script

--- scripts/download_kitti.sh
+++ scripts/download_kitti.sh
@@ -6,9 +6,9 @@ wget https://s3.eu-central-1.amazonaws.com/avg-kitti/data_semantics.zip
 
 # unpacking
 echo "unpacking kitti..."
-unar -d data_semantics.zip
+unzip data_semantics.zip -d data_semantics
 
 # create val set
-mkdir -p data_semantics/val/{image_2, semantic_rgb}
+mkdir -p data_semantics/val/{image_2,semantic_rgb}
 cp data_semantics/training/image_2/000160_10.png data_semantics/val/image_2/000160_10.png
 cp data_semantics/training/semantic_rgb/000160_10.png data_semantics/val/semantic_rgb/000160_10.png
