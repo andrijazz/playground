72c382a719c95e0ed5dc9c225130fc8bed8da283
andrijazz <a.djurisic@yahoo.com>
2021-08-02 13:08:05 +0200

    vanillagan - wip

--- projects/vanillagan/README.md
+++ projects/vanillagan/README.md
@@ -1,11 +1,28 @@
 ## Generative Adversarial Networks
 
-Single-file pytorch implementation of paper [Generative Adversarial Networks](https://arxiv.org/abs/1406.2661)
+Single-file pytorch implementation of paper [Generative Adversarial Networks](https://arxiv.org/abs/1406.2661).
 
 ### TODO
 
+- [X] Math doc
 - [ ] Working version
-- [ ] Math doc 
-- [ ] FID
-- [ ] Inception score
+- [ ] Evaluation - FID
+- [ ] Evaluation - Inception score
 - [ ] Results
+
+### References
+
+```bibtex
+@misc{goodfellow2014generative,
+      title={Generative Adversarial Networks}, 
+      author={Ian J. Goodfellow and Jean Pouget-Abadie and Mehdi Mirza and Bing Xu and David Warde-Farley and Sherjil Ozair and Aaron Courville and Yoshua Bengio},
+      year={2014},
+      eprint={1406.2661},
+      archivePrefix={arXiv},
+      primaryClass={stat.ML}
+}
+```
+### Other resources
+* [Andrew Ng's explanation of BCE Loss](https://www.youtube.com/watch?v=HIQlmHxI6-0)
+* [Information Theory Basics](https://www.youtube.com/watch?v=ErfnhcEV1O8)
+* [Yannic Kilcher's explanation of the paper](https://www.youtube.com/watch?v=eyxmSmjmNS0)
--- projects/vanillagan/vanillagan.py
+++ projects/vanillagan/vanillagan.py
@@ -1,6 +1,46 @@
 import torch
 import torch.nn as nn
+from collections import OrderedDict
+import torch.nn.functional as F
 
 
-class VanillaGAN(nn.Module):
-    pass
+class Maxout(nn.Module):
+    def __init__(self, k):
+        super(Maxout, self).__init__()
+        self.k = k
+
+    def forward(self, x):
+        # assert x.shape
+        torch.max(x)
+
+
+class Generator(nn.Module):
+    def __init__(self, z_dim=8000, hidden_dim=3072):
+        super(Generator, self).__init__()
+        self.z_dim = z_dim
+        layers = [
+            ('dense0', nn.Linear(self.z_dim, hidden_dim)),
+            ('dense0_act', nn.Sigmoid()),
+            ('dense1', nn.Linear(self.hidden_dim, hidden_dim))
+        ]
+
+        self.net = nn.Sequential(OrderedDict(layers))
+
+    def forward(self, x):
+        return self.net(x)
+
+
+class Discriminator(nn.Module):
+    def __init__(self, input_dim=3072, hidden_dim=3072):
+        super(Discriminator, self).__init__()
+        self.input_dim = input_dim
+        layers = [
+            ('dense0', nn.Linear(self.input_dim, hidden_dim)),
+            ('dense0_act', nn.Sigmoid()),
+            ('dense1', nn.Linear(self.hidden_dim, hidden_dim))
+        ]
+
+        self.net = nn.Sequential(OrderedDict(layers))
+
+    def forward(self, x):
+        return self.net(x)
